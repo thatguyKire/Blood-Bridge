98209f2b3d9a2644b6b3fceadf784cca1f6afca6
Lue <escasinasluezyldominic@gmail.com>
2025-09-30 18:56:44 +0800

    synced settings.py to the remote main's settings.py

--- bloodbridge/bloodbridge/settings.py
+++ bloodbridge/bloodbridge/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'bloodbridge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    "default": dj_database_url.config(
-        default="sqlite:///db.sqlite3",
-        conn_max_age=600, # persistent connections
-        ssl_require=True # enforce SSL
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
