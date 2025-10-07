88a46fd962a1dcf6edcc0f588c1c4125d88db3d3
Mirabed <becerairian54@gmail.com>
2025-10-08 02:55:44 +0800

    feature/accounts/add-success-overlay-and-login-modal-popup

--- bloodbridge/accounts/urls.py
+++ bloodbridge/accounts/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
+    path("", views.index_view, name="index"),  # landing page (login/signup)
+    path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("home/", views.home_view, name="home"),  # user homepage after login
 ]
