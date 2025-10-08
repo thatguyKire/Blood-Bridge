c4d3060eb799eaafc34073362fdf5706f51c64f3
Lue <escasinasluezyldominic@gmail.com>
2025-10-08 20:23:52 +0800

    setup(bloodbridge): set database in settings.py to our supabase database

--- bloodbridge/bloodbridge/settings.py
+++ bloodbridge/bloodbridge/settings.py
@@ -74,11 +74,24 @@ WSGI_APPLICATION = 'bloodbridge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+
+from pathlib import Path
+import dj_database_url # type: ignore
+from dotenv import load_dotenv # type: ignore
+
+#load environment variables from .env file
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+#get the database url from environment variable
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600,
+        ssl_require=True # Enforce SSL connection
+    )
 }
 
 
